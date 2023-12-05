138ad71cb2d388d50ad7cb9c624ad7dc5d53f467
JCEOZ <jaroslawzagloba@gmail.com>
2023-12-05 17:24:25 +0100

    Weather Forecast (Comments concept) solution

--- weather-forecast/weather_forecast.go
+++ weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather provides the current weather condition of various cities in Goblinocus.
 package weather
 
+// CurrentCondition represents current weather conditions in provided location.
 var CurrentCondition string
+
+// CurrentLocation represents a location for which the weather condition should be calculated.
 var CurrentLocation string
 
+// Forecast returns a current weather condition for provided city.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
