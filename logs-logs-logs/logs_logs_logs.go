d5e9b89a68fab9ad5acdcf333fd6e2641a03c7e3
JCEOZ <jaroslawzagloba@gmail.com>
2023-12-11 19:34:45 +0100

    Logs, Logs, Logs! (runes concept) solution

--- logs-logs-logs/logs_logs_logs.go
+++ logs-logs-logs/logs_logs_logs.go
@@ -2,17 +2,38 @@ package logs
 
 // Application identifies the application emitting the given log.
 func Application(log string) string {
-	panic("Please implement the Application() function")
+	runes := []rune(log)
+
+	for _, logRune := range runes {
+		switch logRune {
+		case '❗':
+			return "recommendation"
+		case '🔍':
+			return "search"
+		case '☀':
+			return "weather"
+		}
+	}
+
+	return "default"
 }
 
 // Replace replaces all occurrences of old with new, returning the modified log
 // to the caller.
 func Replace(log string, oldRune, newRune rune) string {
-	panic("Please implement the Replace() function")
+	runes := []rune(log)
+	for index, logRune := range runes {
+		if logRune == oldRune {
+			runes[index] = newRune
+		}
+	}
+
+	return string(runes)
 }
 
 // WithinLimit determines whether or not the number of characters in log is
 // within the limit.
 func WithinLimit(log string, limit int) bool {
-	panic("Please implement the WithinLimit() function")
+	runes := []rune(log)
+	return len(runes) <= limit
 }
