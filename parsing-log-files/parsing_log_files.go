3b1c1fcbbf301d9c1341547ac0b126da36f6ece3
JCEOZ <jaroslawzagloba@gmail.com>
2023-12-12 19:21:36 +0100

    Parsing Log Files (regular expression concept) solution

--- parsing-log-files/parsing_log_files.go
+++ parsing-log-files/parsing_log_files.go
@@ -1,21 +1,59 @@
 package parsinglogfiles
 
+import "regexp"
+
 func IsValidLine(text string) bool {
-	panic("Please implement the IsValidLine function")
+	pattern := `\[TRC\]|\[DBG\]|\[INF\]|\[WRN\]|\[ERR\]|\[FTL\]`
+	re := regexp.MustCompile(pattern)
+	locationSlice := re.FindStringIndex(text)
+	if locationSlice == nil {
+		return false
+	}
+
+	return locationSlice[0] == 0
 }
 
 func SplitLogLine(text string) []string {
-	panic("Please implement the SplitLogLine function")
+	pattern := `<[~*=\-]*>`
+	re := regexp.MustCompile(pattern)
+	return re.Split(text, -1)
 }
 
 func CountQuotedPasswords(lines []string) int {
-	panic("Please implement the CountQuotedPasswords function")
+	pattern := `"[^"]*password[^"]*"`
+	re := regexp.MustCompile("(?i)" + pattern)
+	counter := 0
+
+	for _, input := range lines {
+		if re.MatchString(input) {
+			counter++
+		}
+	}
+
+	return counter
 }
 
 func RemoveEndOfLineText(text string) string {
-	panic("Please implement the RemoveEndOfLineText function")
+	pattern := `end-of-line\d+`
+	re := regexp.MustCompile(pattern)
+
+	return re.ReplaceAllString(text, "")
 }
 
 func TagWithUserName(lines []string) []string {
-	panic("Please implement the TagWithUserName function")
+	pattern := `User\s+([A-Za-z0-9]+)`
+	re := regexp.MustCompile(pattern)
+	var result []string
+
+	for _, line := range lines {
+		match := re.FindStringSubmatch(line)
+		output := line
+		if len(match) > 1 {
+			output = "[USR] " + match[1] + " " + line
+		}
+
+		result = append(result, output)
+	}
+
+	return result
 }
