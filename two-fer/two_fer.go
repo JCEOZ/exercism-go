48eb62c3b8e438c57f1e50046deffd2e3663538b
JCEOZ <jaroslawzagloba@gmail.com>
2023-12-13 18:12:50 +0100

    Two Fer exercise solution

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -1,15 +1,10 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package twofer should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package twofer contains the solution for Two Fer exercise from https://exercism.org/tracks/go/exercises/two-fer
 package twofer
 
-// ShareWith should have a comment documenting it.
+// ShareWith generates the phrase which you say when you share a cookie with someone.
 func ShareWith(name string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	if name == "" {
+		name = "you"
+	}
+	return "One for " + name + ", one for me."
 }
