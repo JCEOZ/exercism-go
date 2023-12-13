dce03e383e624f684c6527e6cfde111273d0702a
JCEOZ <jaroslawzagloba@gmail.com>
2023-12-13 18:26:27 +0100

    Raindrops exercise solution

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -1,5 +1,27 @@
+// Package raindrops contains the solution for Raindrops exercise from https://exercism.org/tracks/go/exercises/raindrops
 package raindrops
 
+import "strconv"
+
+// Convert converts a number into string containing raindrop sound
 func Convert(number int) string {
-	panic("Please implement the Convert function")
+	var result string
+
+	if number%3 == 0 {
+		result += "Pling"
+	}
+
+	if number%5 == 0 {
+		result += "Plang"
+	}
+
+	if number%7 == 0 {
+		result += "Plong"
+	}
+
+	if result == "" {
+		result = strconv.Itoa(number)
+	}
+
+	return result
 }
