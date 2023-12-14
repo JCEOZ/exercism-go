5dc4c2c30db53746fcb31a41d6480b8001d5402d
JCEOZ <jaroslawzagloba@gmail.com>
2023-12-14 19:30:04 +0100

    Collatz Conjecture exercise solution

--- collatz-conjecture/collatz_conjecture.go
+++ collatz-conjecture/collatz_conjecture.go
@@ -1,5 +1,23 @@
+// Package collatzconjecture contains the solution for Raindrops exercise from https://exercism.org/tracks/go/exercises/collatz-conjecture
 package collatzconjecture
 
+import "errors"
+
+// CollatzConjecture calculates number of steps required to solve Collatz Conjecture problem
 func CollatzConjecture(n int) (int, error) {
-	panic("Please implement the CollatzConjecture function")
+
+	if n <= 0 {
+		return 0, errors.New("input number must be positive")
+	}
+
+	var numberOfSteps int
+	for ; n != 1; numberOfSteps++ {
+		if n%2 == 0 {
+			n = n / 2
+		} else {
+			n = 3*n + 1
+		}
+	}
+
+	return numberOfSteps, nil
 }
