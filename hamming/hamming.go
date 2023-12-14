ff07dde7a2c5d6ed6bc84b42118539de4578c4fe
JCEOZ <jaroslawzagloba@gmail.com>
2023-12-14 19:57:33 +0100

    Hamming exercise solution

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,5 +1,21 @@
+// Package hamming contains the solution for Hamming exercise from https://exercism.org/tracks/go/exercises/hamming
 package hamming
 
+import "errors"
+
+// Distance calculates the distance between two DNA strands
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	if len(a) != len(b) {
+		return 0, errors.New("provided sequences must have equal length")
+	}
+
+	var distance int
+
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			distance++
+		}
+	}
+
+	return distance, nil
 }
