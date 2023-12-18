35e29be804941a297b7d42af1d5535465d0bf007
JCEOZ <jaroslawzagloba@gmail.com>
2023-12-18 19:08:13 +0100

    Isogram exercise solution

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,5 +1,23 @@
+// Package isogram contains the solution for Isogram exercise from https://exercism.org/tracks/go/exercises/isogram
 package isogram
 
+import (
+	"strings"
+	"unicode"
+)
+
+// IsIsogram checks if provided word is isogram
 func IsIsogram(word string) bool {
-	panic("Please implement the IsIsogram function")
+	characterToOccurrences := make(map[rune]int)
+
+	for _, character := range strings.ToLower(word) {
+		if unicode.IsLetter(character) {
+			if characterToOccurrences[character] > 0 {
+				return false
+			}
+			characterToOccurrences[character]++
+		}
+	}
+
+	return true
 }
